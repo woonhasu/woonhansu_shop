522becbe28fc4f0b387d512a1bd2bf7be96ee64c
geesuee <baejisu97@gmail.com>
2021-09-10 00:38:21 +0900

    [fix] dml 수정

--- sql/dml.sql
+++ sql/dml.sql
@@ -1,6 +1,10 @@
 INSERT INTO product VALUES(product_SEQ.NEXTVAL, 'bottom', '코튼팬츠', 40000, 'beige', 'free');
 INSERT INTO product VALUES(product_SEQ.NEXTVAL, 'setup', '올블랙 셋업', 68000, 'black', 'free');
 INSERT INTO product VALUES(product_SEQ.NEXTVAL, 'top', '스트라이프 셔츠', 34000, 'blue', 'free');
+INSERT INTO product VALUES(product_SEQ.NEXTVAL, 'outer', '숏카라 코트', 48000, 'blue', 'free');
+INSERT INTO product VALUES(product_SEQ.NEXTVAL, 'top', '후드 아노락', 36000, 'khaki', 'free');
+INSERT INTO product VALUES(product_SEQ.NEXTVAL, 'top', '그레이 후드티', 30000, 'grey', 'free');
+INSERT INTO product VALUES(product_SEQ.NEXTVAL, 'bottom', '베이지 코튼 스커트', 32000, 'beige', 'free');
 
 INSERT INTO users VALUES('admin', 'adminpw', '1', '관리자', '관리자집', '01012345678');
 INSERT INTO users VALUES('geesuee', 'jisu', '0', '배지수', '인천 중구 하늘별빛로 121', '01030576577');
